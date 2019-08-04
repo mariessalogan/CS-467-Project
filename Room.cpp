9bc16457a1a6586e82503025df5a97c9c8df2664
mariessalogan <38187604+mariessalogan@users.noreply.github.com>
2019-08-04 11:55:34 -0700

    Big updates
    added setters/ reorganized so it's easier to read

--- Room.cpp
+++ Room.cpp
@@ -12,15 +12,8 @@ using namespace std;
 
 //Room constructor
 Room::Room(){
-  name = "Bunk";
-  shortDesc = "There is a dresser across the door, a picture frame on the floor, and an employee manual on the floor.";
-  longDesc = "\"WARNING! WARNING! OXYGEN LEVELS DROPPING TO CRITICAL LEVEL! PLEASE EVACUATE TO THE NEAREST ESCAPE POD\" You groggily awaken to the sound of a robotic voice booming over the PA system.  You look around and see that the bunk room is in disarray. Other bunks are tipped over, dressers have come apart, and Lopez’ angel figurines are shattered on the ground. You are wedged between your bunk and the wall, a lamp with blood lies directly next to your head.  You touch your forehead and wince, the lamp must have hit you while you slept, possibly causing a minor concussion.  There is blood on your face, but it seems the wound has clotted and is no longer bleeding.  There is a dresser laying across the door to the break room. You will have to push it out of the way to get through the door.  You see a cracked frame on the floor.  Your employee manual is sticking out from a pile of rubble.\n";
-  visited = 0;
+  visited = false;
   itemCount = 0;
-  //for(int i = 0; i < itemCount; i++)
-  //{
-    //itemInventory[i] = itemInput[i];
-  //}
   itemInventory[0] = NULL;
   features[0] = NULL;
   features[1] = NULL;
@@ -31,6 +24,105 @@ Room::Room(){
 
 }
 
+/*************************************************************
+*Setter functions
+*
+**************************************************************/
+void Room::setName(string roomName)
+{
+  name = roomName;
+}
+
+void Room::setLongDesc(string desc)
+{
+  longDesc = desc;
+}
+
+void Room::setShortDesc(string desc)
+{
+  shortDesc = desc;
+}
+
+void Room::setVisited(bool value)
+{
+  visited = value;
+}
+
+void Room::setNorth(Room *newRoom)
+{
+  north = newRoom;
+}
+
+void Room::setSouth(Room *newRoom)
+{
+  south = newRoom;
+}
+
+void Room::setEast(Room *newRoom)
+{
+  east = newRoom;
+}
+
+void Room::setWest(Room *newRoom)
+{
+  west = newRoom;
+}
+
+void Room::setFeature(Item *feature)
+{
+  if(features[0] == NULL)
+  {
+    features[0] = feature;
+  }
+  else
+  {
+    features[1] = feature;
+  }
+}
+
+void Room::setItem(Item *item)
+{
+  itemInventory[itemCount] = item;
+}
+
+void Room::setItemCount(int count){
+  itemCount = count;
+  return;
+}
+
+//Find item in itemInventory that matches the takeName. When found
+//set that slot to null and decrement itemCount for room. Used
+//when player picks up an item. 
+void Room::setItemToNull(string takeName){
+  for(int i = 0 ; i < 8 ; i++){
+    
+    if(takeName == itemInventory[i]->getName()){
+      itemInventory[i] = NULL;
+      itemCount--;
+      break;
+    }
+  
+  }
+  return;
+}
+
+//Find an empty slot in the room itemInventory. When found set that slot to
+//point to the dropped item. Increment itemCount for room. Used when player
+//drops and item. 
+void Room::setItemToPointer(Item * dropItem){
+  for(int i = 0; i < 8 ; i++){
+    if(itemInventory[i] == NULL){
+      itemInventory[i] = dropItem;
+      itemCount++;
+      break;
+    }
+  }
+  return;
+}
+/*************************************************************
+*Getter functions
+*
+**************************************************************/
 string Room::getName(){
   return name;
 }
@@ -110,11 +202,6 @@ int Room::getItemCount(){
 }
 
 
-void Room::setItemCount(int count){
-  itemCount = count;
-  return;
-}
-
 //This may need to be reworked 
 void Room::printItemNames(){
   
@@ -138,39 +225,4 @@ void Room::printFeatureNames(){
       cout << "You see a " << features[i]->getName() << ". ";
     }
   }
-}
-
-
-//Find item in itemInventory that matches the takeName. When found
-//set that slot to null and decrement itemCount for room. Used
-//when player picks up an item. 
-void Room::setItemToNull(string takeName){
-  for(int i = 0 ; i < 8 ; i++){
-    
-    if(takeName == itemInventory[i]->getName()){
-      itemInventory[i] = NULL;
-      itemCount--;
-      break;
-    }
-  
-  }
-  return;
-}
-
-//Find an empty slot in the room itemInventory. When found set that slot to
-//point to the dropped item. Increment itemCount for room. Used when player
-//drops and item. 
-void Room::setItemToPointer(Item * dropItem){
-  for(int i = 0; i < 8 ; i++){
-    if(itemInventory[i] == NULL){
-      itemInventory[i] = dropItem;
-      itemCount++;
-      break;
-    }
-  }
-  return;
-}
-
-void Room::setVisited(bool value){
-  visited = value;
 }
