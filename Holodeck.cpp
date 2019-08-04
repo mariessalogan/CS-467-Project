fabd2278cb73e0b9816158fd758f1b5eaf2e8d50
mariessalogan <38187604+mariessalogan@users.noreply.github.com>
2019-08-04 13:16:39 -0700

    Update Holodeck.cpp

--- Holodeck.cpp
+++ Holodeck.cpp
@@ -9,14 +9,14 @@ This is the implementation of the holodeck that will affect the oxygen level
 #include "Holodeck.hpp"
 using namespace std;
 
-  void Holodeck::actionFunction(Gamestate *G)
+  void Holodeck::actionFunction(GameState *G)
   {
     if(pushed == false)
     {
       //find out current oxygen on gamestate and add 5
-      int oxygen = G.getOxygen();
+      int oxygen = G->getOxygen();
       oxygen += 5;
-      G.setOxygen(oxygen);
+      G->setOxygen(oxygen);
       //set button pushed to true so it knows to not increment by 5 again.
       pushed = true;
     }
