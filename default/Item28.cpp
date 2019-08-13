de9f1afa32d99eca1dcfad89bdf28338958126a3
mariessalogan <38187604+mariessalogan@users.noreply.github.com>
2019-08-12 19:51:23 -0700

    Create Item28.cpp

---
+++ default/Item28.cpp
@@ -0,0 +1,7 @@
+transporter
+You look at the transporter’s console.  It looks like a large claw or blade slashed it down the middle.  Whoever destroyed it wanted to make sure that it was useless to any one trying to escape.  Every once in a while sparks emit from the console.  You look at the glass and wonder what could have shattered the tube.  It was extremely thick glass, any glass that was thin or had flaws would have immediately broken under the pressure of its first transport. 
+NULL
+false
+NULL
+true
+transporter room
